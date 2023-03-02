b6860fea266370c1b0dbc27130969125b37216f0
Daniel Correa <yo@danielgara.com>
2023-03-02 16:22:34 -0500

    first commit

---
+++ src/controllers/TodoController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import Todo from '../models/Todo';
+
+export default class TodoController {
+  public static async listClosed(req: Request, res: Response) {
+    const closedTodos = await Todo.findAll({ where: { 'state': 'CLOSED' } });
+    res.json({ "list": closedTodos });
+  }
+
+  public static async listOpen(req: Request, res: Response) {
+    const openTodos = await Todo.findAll({ where: { 'state': 'OPEN' } });
+    res.json({ "list": openTodos });
+  }
+
+  public static async create(req: Request, res: Response) {
+    await Todo.create({
+      message: req.body.message,
+      state: "OPEN"
+    });
+    res.json({ "res": "Todo created successfully" });
+  }
+
+  public static async delete(req: Request, res: Response) {
+    const id = req.params.id;
+    await Todo.destroy({ where: { id: id } });
+    res.json({ "res": "Todo deleted successfully" });
+  }
+
+  public static async update(req: Request, res: Response) {
+    const id = req.params.id;
+    const state = req.params.state;
+    await Todo.update({state: state}, { where: { id: id } });
+    res.json({ "res": "Todo updated successfully" });
+  }
+}
---
+++ src/models/Todo.ts
@@ -0,0 +1,18 @@
+import { Sequelize, DataTypes, Model } from 'sequelize';
+
+export default class Todo extends Model {
+  public static initialize(sequelize: Sequelize){
+    Todo.init({
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      message: DataTypes.STRING,
+      state: DataTypes.STRING,
+    }, {
+      sequelize,
+      modelName: 'Todo'
+    });
+  }
+}
